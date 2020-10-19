73af0c41867fdeb0022bd76cd97f7e1ba7006c14
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-10-19 20:43:21 +0600

    Programming Assignment: Support Vector Machines Done

--- Programming Assignments/machine-learning-ex6/ex6/dataset3Params.m
+++ Programming Assignments/machine-learning-ex6/ex6/dataset3Params.m
@@ -24,9 +24,25 @@ sigma = 0.3;
 %
 
 
+err = 1.0;
 
+list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+for i = 1 : length(list)
+    for j = 1 : length(list)
+        C1 = list(i);
+        sigma1 = list(j);
 
+        model = svmTrain(X, y, C1, @(x1, x2) gaussianKernel(x1, x2, sigma1));
+        predictions = svmPredict(model, Xval);
 
+        err1 = mean(double(predictions ~= yval));
+        if err1 < err
+                C = C1;
+                sigma = sigma1;
+                err = err1;
+        end
+    end
+end
 
 
 % =========================================================================
--- Programming Assignments/machine-learning-ex6/ex6/emailFeatures.m
+++ Programming Assignments/machine-learning-ex6/ex6/emailFeatures.m
@@ -51,7 +51,9 @@ x = zeros(n, 1);
 
 
 
-
+for i = 1 : length(word_indices)
+    x(word_indices(i)) = 1;
+end
 
 
 
